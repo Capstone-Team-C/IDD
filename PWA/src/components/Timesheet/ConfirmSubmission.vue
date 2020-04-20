33fcc15d6b85e4b118b16381651dd19964580688
Keaton Kraiger <kkraiger@pdx.edu>
2020-04-19 20:11:06 -0700

    Configured code formater.
    Signed-off-by: Keaton Kraiger <kkraiger@pdx.edu>

--- PWA/src/components/Timesheet/ConfirmSubmission.vue
+++ PWA/src/components/Timesheet/ConfirmSubmission.vue
@@ -1,6 +1,10 @@
 <template>
   <v-row justify="center">
-    <v-btn color="success" dark @click.stop="dialog = true" @click="validate">
+    <v-btn 
+    color="success" 
+    dark 
+    @click.stop="dialog = true" 
+    @click="validate">
       Submit
     </v-btn>
 
@@ -20,11 +24,17 @@
             <v-card-actions>
               <v-spacer></v-spacer>
 
-							<!-- Confirm if user is ready to submit -->
-              <v-btn color="red" text @click="dialog = false">
+              <!-- Confirm if user is ready to submit -->
+              <v-btn 
+              color="red" 
+              text 
+              @click="dialog = false">
                 Cancel
               </v-btn>
-              <v-btn color="green darken-1" text @click="submit">
+              <v-btn 
+              color="green darken-1" 
+              text 
+              @click="submit">
                 Submit
               </v-btn>
             </v-card-actions>
@@ -32,21 +42,20 @@
 
           <div v-else>
             <div v-if="!returnHome">
-
-							<!-- Submitting the form -->
+              <!-- Submitting the form -->
               <div class="text-center">
                 <v-progress-circular
                   :size="50"
                   color="primary"
                   indeterminate
-									id="progress"
+                  id="progress"
                 ></v-progress-circular>
                 <p class="text--disabled">Submitting form</p>
               </div>
             </div>
 
             <div v-else>
-						<!-- Display submission status -->
+              <!-- Display submission status -->
               <div v-if="submissionStatus">
                 <v-card-title class="headline text-center" id="submited"
                   >Your form has been submitted!</v-card-title
@@ -69,19 +78,19 @@
           </div>
         </v-card>
       </div>
-				
-			<!-- The form is not valid -->
-			<div v-else>
-				<v-card>
-					<v-card-title class="headline text-danger" id="invalid">
-						Your form is not valid.</v-card-title
-					>
-	
-					<v-card-text>
-						Please fix the invalid parts of the form and then retry submitting
-						your form.
-					</v-card-text>
-				</v-card>
+
+      <!-- The form is not valid -->
+      <div v-else>
+        <v-card>
+          <v-card-title class="headline text-danger" id="invalid">
+            Your form is not valid.</v-card-title
+          >
+
+          <v-card-text>
+            Please fix the invalid parts of the form and then retry submitting
+            your form.
+          </v-card-text>
+        </v-card>
       </div>
     </v-dialog>
   </v-row>
@@ -109,13 +118,13 @@
   export default {
     name: "ConfirmSubmission",
     props: {
-			//If the information is valid.
+      //If the information is valid.
       valid: {
         type: Boolean,
         default: false,
       },
 
-			//User (edited) information.
+      //User (edited) information.
       formFields: {
         type: Object,
         default: null,
@@ -124,24 +133,23 @@
 
     data() {
       return {
-				//If the dialog is still up.
+        //If the dialog is still up.
         dialog: false,
 
-				//Log of POST connection.
+        //Log of POST connection.
         loading: false,
 
-				//Track when the POST completes
+        //Track when the POST completes
         submissionStatus: false,
 
-				//Flag for once POST has been successful/failed
+        //Flag for once POST has been successful/failed
         returnHome: false,
 
-				//Data to be submitted 
-				submitData: null,
+        //Data to be submitted
+        submitData: null,
 
-				//URL for the AppServer
-				url: process.env.VUE_APP_SERVER_URL.concat('Submit'),
-
+        //URL for the AppServer
+        url: process.env.VUE_APP_SERVER_URL.concat("Submit"),
       };
     },
 
@@ -151,8 +159,8 @@
           console.log("Valid form");
         }
       },
-			
-			//Formats the data to be posted
+
+      //Formats the data to be posted
       formatData() {
         var submitData = {};
         Object.entries(this.formFields).forEach(([key, value]) => {
@@ -176,37 +184,37 @@
             submitData["serviceDeliveredOn"]["value"].push(row);
           }
         );
-				this.submitData=submitData;
+        this.submitData = submitData;
       },
 
       //Submits form to AppServer.
       submit() {
-			//After form is validated, post timesheet.
-			this.loading = true;
-			var self = this;
-			//Prepare the data to send.
-			this.formatData();
+        //After form is validated, post timesheet.
+        this.loading = true;
+        var self = this;
+        //Prepare the data to send.
+        this.formatData();
 
-			if (this.valid) {
-				axios
-					.post(this.url, this.submitData, {
-						headers: {
-							"content-type": "text/plain",
-						},
-					})
-					.then(function (response) {
-						if (response["data"]["response"] == "ok") {
-							console.log("Finished posting!");
-							self.submissionStatus = true;
+        if (this.valid) {
+          axios
+            .post(this.url, this.submitData, {
+              headers: {
+                "content-type": "text/plain",
+              },
+            })
+            .then(function (response) {
+              if (response["data"]["response"] == "ok") {
+                console.log("Finished posting!");
+                self.submissionStatus = true;
 
-							//Return to home here?
-							self.returnHome = true;
-						}
-					})
-					.catch(function (error) {
-						console.log(error);
-					});
-			}
+                //Return to home here?
+                self.returnHome = true;
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }
       },
     },
   };
