cdbf18db8433dded2cdededf848d40cdd5f7d2eb
brialee <brialee@pdx.edu>
2020-06-06 14:14:33 -0700

    Appserver controller filename changes. Adds comments, removes unused code and dependencies from said files.

---
+++ Appserver/Controllers/DocumentSubmissionController.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Appserver.Data;
+using Appserver.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Common.Models;
+using IDD;
+using Common.Data;
+
+namespace Appserver.Controllers
+{
+    public class TimesheetController : Controller
+    {
+        /*******************************************************************************
+        /// Fields
+        *******************************************************************************/
+        private readonly SubmissionStagingContext _context;
+        private readonly SubmissionContext _subcontext;
+
+        /*******************************************************************************
+        /// Constructor
+        *******************************************************************************/
+        public TimesheetController(SubmissionStagingContext context, SubmissionContext subcontext)
+        {
+            _context = context;
+            _subcontext = subcontext;
+        }
+
+
+        /*******************************************************************************
+        /// Methods
+        *******************************************************************************/
+
+
+        // Takes the staging id from a previous upload operation. 
+        [Produces("application/json")]
+        [Route("Timesheet/ReadyTest")]
+        [HttpGet]
+        public IActionResult ReadyTest(int id)
+        {
+            var result = _context.Stagings.FirstOrDefault(m=> m.Id == id );
+            if( result == null)
+            {
+                return Json(new JsonResponse("not ready"));
+            }
+            return Ready(id, result.Guid);
+        }
+
+        // A previous upload is considered ready when the response from textract has
+        // been received. When ready, a json repersentation of the appropriate form is returned.
+        [Produces("application/json")]
+        [HttpGet]
+        public IActionResult Ready(int id, string guid)
+        {
+            var stage = _context.Stagings.FirstOrDefault(m => m.Id == id && m.Guid == guid);
+
+            if (stage == null)
+            {
+                return Json(new JsonResponse("not ready"));
+            }
+
+            var textractform = new TextractDocument.TextractDocument();
+
+            foreach (var a in JArray.Parse(stage.ParsedTextractJSON))
+            {
+                var childform = new TextractDocument.TextractDocument();
+                childform.FromJson(a);
+                textractform.AddPages(childform);
+            }
+
+            AbstractFormObject form;
+
+            try
+            {
+                form = AbstractFormObject.FromTextract(textractform, stage.formType);
+            }
+            catch (Exception)
+            {
+                return Json(new JsonResponse("invalid"));
+            }
+
+            form.id = id;
+            form.guid = guid;
+
+            return Json(form);
+        }
+
+
+        // Used primarily for testing and validation
+        [Route("Timesheet/SubmitTest")]
+        [HttpGet]
+        [Produces("application/json")]
+        public IActionResult Submit(int id)
+        {
+            var oldstage = _context.Stagings.FirstOrDefault(m => m.Id == id);
+
+            if (oldstage == null)
+            {
+                return Json(new JsonResponse("not ready"));
+            }
+
+            // Copy staging
+            var stage = new SubmissionStaging {
+                Guid = Guid.NewGuid().ToString(),
+                UriString = oldstage.UriString,
+                ParsedTextractJSON = oldstage.ParsedTextractJSON,
+                formType = oldstage.formType
+                };
+            _context.Add(stage);
+            _context.SaveChanges();
+
+            var textractform = new TextractDocument.TextractDocument();
+
+            foreach (var a in JArray.Parse(stage.ParsedTextractJSON))
+            {
+                var childform = new TextractDocument.TextractDocument();
+                childform.FromJson(a);
+                textractform.AddPages(childform);
+            }
+            AbstractFormObject ts;
+            try
+            {
+                ts = AbstractFormObject.FromTextract(textractform, stage.formType);
+            }
+            catch (Exception)
+            {
+                return Json(new JsonResponse("invalid form"));
+            }
+
+            ts.id = stage.Id;
+            ts.guid = stage.Guid;
+
+            var PWAForm = PWAsubmission.FromForm(ts, stage.formType);
+            switch (stage.formType) {
+                case AbstractFormObject.FormType.OR526_ATTENDANT:
+                case AbstractFormObject.FormType.OR507_RELIEF:
+                    return SubmitTimesheet((PWATimesheet)PWAForm);
+                case AbstractFormObject.FormType.OR004_MILEAGE:
+                    return SubmitMileage((PWAMileage)PWAForm);
+                default:
+                    return Json(new
+                    {
+                        response = "invalid"
+                    }
+            );
+            }
+        }
+
+
+        // Takes a Timesheet submitted by the PWA after the user has had a chance
+        // to review the processed upload and possibly make edits. Converts the
+        // PWATimesheet model into a Timesheet model which can be placed into the submissions
+        // table. 
+        [Route("Timesheet/Submit")]
+        [HttpPost("Submit")]
+        [Produces("application/json")]
+        public IActionResult SubmitTimesheet([FromBody] PWATimesheet submittedform)
+        {
+            // Check correct authorization
+            var result = _context.Stagings.FirstOrDefault(m => m.Id == submittedform.id && m.Guid == submittedform.guid);
+            if( result == null)
+            {
+                return Json(new
+                {
+                    response = "invalid"
+                });
+            }
+
+            var dbutil = new FormToDbUtil(_subcontext, _context);
+            Timesheet ts = dbutil.PopulateTimesheet(submittedform);
+
+            var submission = _subcontext;
+            using (var transaction = submission.Database.BeginTransaction())
+            {
+                submission.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Submissions ON");
+                submission.Add(ts);
+                submission.SaveChanges();
+                transaction.Commit();
+            }
+
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            return Json(new {response="ok"});
+        }
+
+
+        // Takes a submission from the PWA modeled by PWAMileage, converts that
+        // into a MileageForm (the model used the DB), and saves it to
+        // the Submissions table.
+        [Route("Timesheet/SubmitMileage")]
+        [HttpPost("Submit")]
+        [Produces("application/json")]
+        public IActionResult SubmitMileage([FromBody] PWAMileage submittedform)
+        {
+            var dbutil = new FormToDbUtil(_subcontext, _context);
+            MileageForm mf = dbutil.PopulateMileage(submittedform);
+
+            var submission = _subcontext;
+            submission.Add(mf);
+            submission.SaveChanges();
+
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            return Json(new { response = "ok" });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        /*******************************************************************************
+        /// Classes
+        *******************************************************************************/
+        private class JsonResponse
+        {
+            public JsonResponse(string res = "ok")
+            {
+                response = res;
+            }
+            public string response;
+        }
+    }
+}
---
+++ Appserver/Controllers/DocumentUploadController.cs
@@ -0,0 +1,188 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Amazon.Textract.Model;
+using System;
+using Appserver.Data;
+using Appserver.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Common.Data;
+using Microsoft.Extensions.Primitives;
+
+namespace Appserver.Controllers
+{
+    public class ImageUploadController : Controller
+    {
+
+        /*******************************************************************************
+        /// Fields
+        *******************************************************************************/
+        private readonly SubmissionStagingContext _context;
+
+        /*******************************************************************************
+        /// Constructor
+        *******************************************************************************/
+        public ImageUploadController(SubmissionStagingContext context)
+        {
+            _context = context;
+        }
+
+
+        /*******************************************************************************
+        /// Methods
+        *******************************************************************************/
+
+        // POST: /home/timesheet/
+        // Handle the initial processing of document uploads. Currently, the only accepted
+        // formats are jpg/png/pdf. Valid documents are sent to AWS Textract for processing
+        // with the results being stored in a staging table. The documents themselves are
+        // stored in Azure Blob upon being uploaded. 
+        [HttpPost("ImageUpload")]
+        public async Task<IActionResult> PostImage(List<IFormFile> files, AbstractFormObject.FormType formType)
+        {
+            var c = files.Count;
+            var image_responses = new List<AnalyzeDocumentResponse>();
+            var pdf_responses = new List<GetDocumentAnalysisResponse>();
+            var skipped_files = new List<string>();
+
+            // MIME types we can send to textract
+            var accepted_types = new List<string>
+            {
+                "image/jpeg",
+                "image/png",
+                "application/pdf",
+            };
+
+            // Iterate of collection of file and send to Textract
+            foreach (var file in files)
+            {
+                // Nothing to work with, next!
+                if(file.Length == 0)
+                {
+                    skipped_files.Add("File name " + file.Name + " is empty");
+                    continue;
+                }
+                // Only process files that have acceptable types
+                if (accepted_types.Contains(file.ContentType))
+                {
+                    // Process PDF
+                    if(file.ContentType == "application/pdf")
+                    {
+                        pdf_responses.Add(process_pdf(file));
+                    }
+                    // Process image
+                    else
+                    {
+                        image_responses.Add(process_image(file));
+                    }
+                }
+                else
+                {
+                    skipped_files.Add(
+                        "File name " + file.Name +
+                        " has incompatible type " + file.ContentType
+                        );
+                }
+            }
+
+            int stageId;
+            string stageGuid = Guid.NewGuid().ToString();
+            if (pdf_responses.Count > 0)
+            {
+                stageId = await saveSubmissionStage(await UploadToBlob(files), pdf_responses, formType, stageGuid);
+            }
+            else
+            {
+                stageId = await saveSubmissionStage(await UploadToBlob(files), image_responses, formType, stageGuid);
+            }
+
+
+            return Json(new
+            {
+                file_count = c,
+                skipped = skipped_files,
+                id = stageId,
+                guid = stageGuid
+            }
+            ) ;
+        }
+
+
+        // Document upload controller that is compatible with the format
+        // being sent from the PWA.
+        [Route("ImageUpload/DocAsForm")]
+        [HttpPost("ImageList")]
+        public async Task<IActionResult> ImageList(IFormCollection file_collection)
+        {
+            if (file_collection["formChoice"].Equals(StringValues.Empty))
+            {
+                return Json(new
+                {
+                    response = "invalid form type"
+                });
+            }
+            AbstractFormObject.FormType formType = (AbstractFormObject.FormType)Enum.Parse(typeof(AbstractFormObject.FormType),file_collection["formChoice"].ToString());
+
+            return await PostImage(file_collection.Files.ToList(), formType);
+        }
+
+        // Store user submissions in Azure and get back the file's bucket handle.
+        private async Task<string> UploadToBlob(List<IFormFile> files)
+        {
+            // Get Blob Container
+            var container = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("BLOB_CONNECTION"))
+                .CreateCloudBlobClient()
+                .GetContainerReference("submissionfiles");
+            await container.CreateIfNotExistsAsync();
+            await container.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Container });
+
+            // Upload images to container and save UriStrings
+            var uriString ="";
+            foreach (var f in files)
+            {
+                if (!string.IsNullOrEmpty(uriString))
+                    uriString += ',';
+                var blockBlob = container.GetBlockBlobReference(DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fffffff") + "_" + f.FileName);
+                uriString += blockBlob.Uri.AbsoluteUri;
+                await blockBlob.UploadFromStreamAsync(f.OpenReadStream());
+            }
+
+            return uriString;
+        }
+
+        // Place yet-to-be-submitted documents, in the form of a reference to a bucket file
+        // and the response from textract, into a staging table.
+        private async Task<int> saveSubmissionStage<T>(string uriString, List<T> responses, AbstractFormObject.FormType formType, string guid)
+        {
+            // Create a SubmissionStaging to upload to SubmissionStaging table
+            var ss = new SubmissionStaging
+            {
+                Guid = guid,
+                ParsedTextractJSON = System.Text.Json.JsonSerializer.Serialize(responses),
+                UriString = uriString,
+                formType = formType
+            };
+
+            // Add SubmissionStaging to table and get the Id to add to JSON response return
+            _context.Add(ss);
+            await _context.SaveChangesAsync();
+            return ss.Id;
+        }
+
+        // Method to handle image format uploads
+        private AnalyzeDocumentResponse process_image(IFormFile file)
+        {
+            return new TextractHandler().HandleAsyncJob(file.OpenReadStream());
+        }
+
+        // Method to handle PDF uploads.
+        private GetDocumentAnalysisResponse process_pdf(IFormFile file)
+        {
+            return new TextractHandler().HandlePDFasync(file);
+        }
+
+    }
+}
